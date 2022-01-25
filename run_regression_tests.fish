2785def66b7ad23accbd3d2b7a68adea146473ba
Jaye Martindell <1649983+DaemonF@users.noreply.github.com>
2022-01-24 17:34:07 -0800

    Add regression tests for Lewdle.

--- run_regression_tests.fish
+++ run_regression_tests.fish
@@ -3,4 +3,7 @@
 for strat in {freq,clues,bifur}
   ./wordle_buddy.py --strategy=$strat -t \
     > regression_tests/$strat/wordle_answers.txt
+  ./wordle_buddy.py --strategy=$strat -t \
+    --dict_file=wordlists/lewdle_answers.txt \
+    > regression_tests/$strat/lewdle_answers.txt
 end
